44484c08396acd83a753186fe47cd873178816e3
Kyle Kretschmer <krtkyl004@myuct.ac.za>
2022-06-18 11:39:37 +0200

    Adding code from previous react app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+import Application from "./Components/Application";
+import UserProvider from "./providers/UserProvider";
+import { BrowserRouter } from "react-router-dom";
 function App() {
-  return <div>Hello</div>;
+  return (
+    <UserProvider>
+      <BrowserRouter>
+        <Application />
+      </BrowserRouter>
+    </UserProvider>
+  );
 }
 
 export default App;
